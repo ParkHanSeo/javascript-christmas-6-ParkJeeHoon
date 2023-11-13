0f3143586fffe816dfc7697b79929395c089f642
ParkJeeHoon <gkstj8300@naver.com>
2023-11-13 13:32:16 +0900

    feat: 사용자 주문메뉴,개수 입력값에 대한 유효성 체크 함수 생성

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,16 +1,28 @@
 import { ERROR_MESSAGE } from "../contants/message.js";
+import { TOTAL_MENU } from "../contants/constants.js"
 
 const Validation = {
     inputVisitScheduleValidate(visitSchedule){
-        if(visitSchedule < 1 || visitSchedule > 31){
+        if(visitSchedule < 1 || visitSchedule > 31)
             throw new Error(`${ERROR_MESSAGE.VISIT_SCHEDULE_ERROR}`);
-        }
-        if(visitSchedule == 0 || visitSchedule == '' || visitSchedule == undefined){
+        if(visitSchedule == 0 || visitSchedule == '' || visitSchedule == undefined)
             throw new Error(`${ERROR_MESSAGE.VISIT_SCHEDULE_ERROR}`);
-        }
-        if(isNaN(visitSchedule)){
+        if(isNaN(visitSchedule))
             throw new Error(`${ERROR_MESSAGE.VISIT_SCHEDULE_ERROR}`);
-        }
+    },
+
+    inputOrderMenuValidate(orderMenu){
+        const duplicateData = [];
+        orderMenu.forEach((data, idx) => {
+            const [menu, quantity] = data.split('-');
+            duplicateData.push(menu);
+            if(!Object.values(TOTAL_MENU).find(data => data.name === menu))
+                throw new Error(`${ERROR_MESSAGE.ORDER_MENU_ERROR}`);
+            if(isNaN(quantity))
+                throw new Error(`${ERROR_MESSAGE.ORDER_MENU_ERROR}`);
+        });
+        if([...new Set(duplicateData)].length !== orderMenu.length)
+            throw new Error(`${ERROR_MESSAGE.ORDER_MENU_ERROR}`);
     }
 }
 
