6a50cd2bf394f3d7f4b82a4a3c012b547b66ac75
ParkJeeHoon <gkstj8300@naver.com>
2023-11-13 13:25:52 +0900

    feat: 메뉴, 뱃지 기준 가격, 날짜, 사용자 output에 대한 상수값 추가

--- src/contants/constants.js
+++ src/contants/constants.js
@@ -1,53 +1,143 @@
 export const APPETIZER = {
-    MUSHROOM_SOUP: '양송이수프', 
-    TAPAS: '타파스', 
-    CAESAR_SALAD: '시저샐러드'
+    MUSHROOM_SOUP: {
+        name: '양송이수프',
+        price: 55000
+    }, 
+    TAPAS: {
+        name: '타파스',
+        price: 5500
+    }, 
+    CAESAR_SALAD: { 
+        name: '시저샐러드',
+        price: 8000
+    }
 }
 
-export const MAIN = {
-    TBONE_STEAK: '티본스테이크', 
-    BBQ_RIBS: '바비큐립', 
-    SEAFOOD_PASTA: '해산물파스타',
-    CHRISTMAS_PASTA: '크리스마스파스타'
+export const MAIN_MENU = {
+    TBONE_STEAK: {
+        name: '티본스테이크',
+        price: 55000
+    },
+    BBQ_RIBS: {
+        name: '바비큐립',
+        price: 54000
+    },
+    SEAFOOD_PASTA: {
+        name: '해산물파스타',
+        price: 35000
+    },
+    CHRISTMAS_PASTA: {
+        name: '크리스마스파스타',
+        price: 25000
+    }
 }
 
-export const DESSERT = {
-    CHOCOLATE_CAKE: '초코케이크',
-    ICE_CREAM: '아이스크림'
+export const DESSERT_MENU = {
+    CHOCOLATE_CAKE: {
+        name: '초코케이크',
+        price: 15000
+    },
+    ICE_CREAM: {
+        name: '아이스크림',
+        price: 5000
+    }
 }
 
-export const BEVERAGE = {
-    ZERO_COLA: '제로콜라',
-    RED_WINE: '레드와인',
-    CHAMPAGNE: '샴페인'
+export const BEVERAGE_MENU = {
+    ZERO_COLA: {
+        name: '제로콜라',
+        price: 3000
+    },
+    RED_WINE: {
+        name: '레드와인',
+        price: 60000
+    },
+    CHAMPAGNE: {
+        name: '샴페인',
+        price: 25000
+    }
 }
 
-export const APPETIZER_PRICE = {
-    MUSHROOM_SOUP: 6000, 
-    TAPAS: 5500, 
-    CAESAR_SALAD: 8000
-}
-
-export const MAIN_PRICE = {
-    TBONE_STEAK: 55000, 
-    BBQ_RIBS: 54000, 
-    SEAFOOD_PASTA: 35000,
-    CHRISTMAS_PASTA: 25000
-}
-
-export const DESSERT_PRICE = {
-    CHOCOLATE_CAKE: 15000,
-    ICE_CREAM: 5000
-}
-
-export const BEVERAGE_PRICE = {
-    ZERO_COLA: 3000,
-    RED_WINE: 60000,
-    CHAMPAGNE: 25000
+export const TOTAL_MENU = {
+    MUSHROOM_SOUP: {
+        name: '양송이수프',
+        price: 55000
+    }, 
+    TAPAS: {
+        name: '타파스',
+        price: 5500
+    }, 
+    CAESAR_SALAD: { 
+        name: '시저샐러드',
+        price: 8000
+    },
+    TBONE_STEAK: {
+        name: '티본스테이크',
+        price: 55000
+    },
+    BBQ_RIBS: {
+        name: '바비큐립',
+        price: 54000
+    },
+    SEAFOOD_PASTA: {
+        name: '해산물파스타',
+        price: 35000
+    },
+    CHRISTMAS_PASTA: {
+        name: '크리스마스파스타',
+        price: 25000
+    },
+    CHOCOLATE_CAKE: {
+        name: '초코케이크',
+        price: 15000
+    },
+    ICE_CREAM: {
+        name: '아이스크림',
+        price: 5000
+    },
+    ZERO_COLA: {
+        name: '제로콜라',
+        price: 3000
+    },
+    RED_WINE: {
+        name: '레드와인',
+        price: 60000
+    },
+    CHAMPAGNE: {
+        name: '샴페인',
+        price: 25000
+    }
 }
 
 export const BADGE = {
     STAR: '별',
     TREE: '트리',
-    SAMTA: '산타'
+    SANTA: '산타'
+}
+
+export const BADGE_AMOUNT = {
+    STAR_AMOUNT: 5000,
+    TREE_AMOUNT: 10000,
+    SANTA_AMOUNT: 20000
+}
+
+export const DATE = {
+    EVENT_MAX_DATE: new Date(2023, 11, 25),
+    EVENT_WEEKDAY: 'wd',
+    EVENT_WEEKEND: 'we'
+}
+
+export const AMOUNT = {
+    CHRISTMAS_START_AMOUNT: 1000,
+    CHRISTMAS_PLUS_AMOUNT: 100,
+    WEEKDAY_START: 0,
+    WEEKDAY_END: 4,
+    WEEKEND_START: 5,
+    WEEKEND_END: 6,
+    WEEKDAY_DISCOUNT: 2023,
+    SPECIAL_DISCOUNT: 1000,
+}
+
+export const EVENT = {
+    GIFT_AMOUNT: 120000
 }
