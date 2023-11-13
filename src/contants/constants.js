185f366ad86ea44e4cb2f4ab8e4f575ad45668a5
ParkJeeHoon <gkstj8300@naver.com>
2023-11-13 13:17:04 +0900

    feat: 메뉴, 가격, 뱃지, 입출력 메시지 상수 생성

---
+++ src/contants/constants.js
@@ -0,0 +1,53 @@
+export const APPETIZER = {
+    MUSHROOM_SOUP: '양송이수프', 
+    TAPAS: '타파스', 
+    CAESAR_SALAD: '시저샐러드'
+}
+
+export const MAIN = {
+    TBONE_STEAK: '티본스테이크', 
+    BBQ_RIBS: '바비큐립', 
+    SEAFOOD_PASTA: '해산물파스타',
+    CHRISTMAS_PASTA: '크리스마스파스타'
+}
+
+export const DESSERT = {
+    CHOCOLATE_CAKE: '초코케이크',
+    ICE_CREAM: '아이스크림'
+}
+
+export const BEVERAGE = {
+    ZERO_COLA: '제로콜라',
+    RED_WINE: '레드와인',
+    CHAMPAGNE: '샴페인'
+}
+
+export const APPETIZER_PRICE = {
+    MUSHROOM_SOUP: 6000, 
+    TAPAS: 5500, 
+    CAESAR_SALAD: 8000
+}
+
+export const MAIN_PRICE = {
+    TBONE_STEAK: 55000, 
+    BBQ_RIBS: 54000, 
+    SEAFOOD_PASTA: 35000,
+    CHRISTMAS_PASTA: 25000
+}
+
+export const DESSERT_PRICE = {
+    CHOCOLATE_CAKE: 15000,
+    ICE_CREAM: 5000
+}
+
+export const BEVERAGE_PRICE = {
+    ZERO_COLA: 3000,
+    RED_WINE: 60000,
+    CHAMPAGNE: 25000
+}
+
+export const BADGE = {
+    STAR: '별',
+    TREE: '트리',
+    SAMTA: '산타'
+}
---
+++ src/contants/message.js
@@ -0,0 +1,18 @@
+export const MESSAGE = {
+    WELCOME_MSG: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
+    EVENT_BENEFIT_MSG: '12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!',
+    TOTAL_MENU_MSG: '<주문 메뉴>',
+    TOTAL_ORDER_MSG: '<할인 전 총주문 금액>',
+    GIFT_MENU_MSG: '<증정 메뉴>',
+    BENEFIT_LIST_MSG: '<혜택 내역>',
+    TOTAL_BENEFIT_AMOUNT_MSG: '<총혜택 금액>',
+    EXPECTED_PAYMENT_AMOUNT_MSG: '<할인 후 예상 결제 금액>',
+    EVENT_BADGE_MSG: '<12월 이벤트 배지>',
+}
+
+export const BENEFIT_MESSAGE = {
+    CHRISTMAS_DDAY_DISCOUNT: '크리스마스 디데이 할인: ',
+    WEEKDAY_DISCOUNT: '평일 할인: ',
+    SPECIAL_DISCOUNT: '특별 할인: ',
+    GIFT_EVENT: '증정 이벤트: '
+}
