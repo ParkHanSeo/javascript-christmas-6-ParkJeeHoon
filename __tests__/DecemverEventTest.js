c1d696a91d667faf3d036f8776685e7bc5b10b87
ParkJeeHoon <gkstj8300@naver.com>
2023-11-15 16:19:59 +0900

    test: DecemberEvent Class 함수 분리를 통한 test코드 수정 및 추가

--- __tests__/DecemverEventTest.js
+++ __tests__/DecemverEventTest.js
@@ -6,45 +6,63 @@ describe("DecemberEvent 클래스 테스트", () => {
     test("증정품 여부 테스트.", () => {
         const decemberEvent = new DecemberEvent();
         const orderMenu = new Order(('해산물파스타-2,레드와인-1,초코케이크-1').split(',')).totalMenuOrderAmount();
-        const discount = new Discount().promotionTotalDiscount(3, orderMenu.ORDER);
-        const giftResult = decemberEvent.giftEventCheck(discount.TOTAL_DISCOUNT, orderMenu.totalAmount);
-        const expected = { giftTotalDiscount: 29223, champagneGift: '샴페인' };
+        const giftResult = decemberEvent.getGiftChampagneCheck(orderMenu.totalAmount);
+        const expected = '샴페인';
         expect(giftResult).toStrictEqual(expected);
     });
 
     test("증정품 없음 테스트.", () => {
         const decemberEvent = new DecemberEvent();
         const orderMenu = new Order(('타파스-1,제로콜라-1').split(',')).totalMenuOrderAmount();
+        const giftResult = decemberEvent.getGiftChampagneCheck(orderMenu.totalAmount);
+        const expected = '';
+        expect(giftResult).toStrictEqual(expected);
+    });
+
+    test("증정품 가격을 합산한 할인 가격 테스트.", () => {
+        const decemberEvent = new DecemberEvent();
+        const orderMenu = new Order(('양송이수프-1,크리스마스파스타-2,아이스크림-2,레드와인-1').split(',')).totalMenuOrderAmount();
         const discount = new Discount().promotionTotalDiscount(26, orderMenu.ORDER);
-        const giftResult = decemberEvent.giftEventCheck(discount.TOTAL_DISCOUNT, orderMenu.totalAmount);
-        const expected = { giftTotalDiscount: 0, champagneGift: '' };
+        const champagneGift = decemberEvent.getGiftChampagneCheck(orderMenu.totalAmount);
+        const giftResult = decemberEvent.getGiftTotalDiscountCheck(discount.TOTAL_DISCOUNT, champagneGift);
+        const expected = 29046;
+        expect(giftResult).toStrictEqual(expected);
+    });
+
+    test("증정품 가격을 합산하지 않는 할인 가격 테스트.", () => {
+        const decemberEvent = new DecemberEvent();
+        const orderMenu = new Order(('양송이수프-1,크리스마스파스타-1,아이스크림-2,레드와인-1').split(',')).totalMenuOrderAmount();
+        const discount = new Discount().promotionTotalDiscount(26, orderMenu.ORDER);
+        const champagneGift = decemberEvent.getGiftChampagneCheck(orderMenu.totalAmount);
+        const giftResult = decemberEvent.getGiftTotalDiscountCheck(discount.TOTAL_DISCOUNT, champagneGift);
+        const expected = 4046;
         expect(giftResult).toStrictEqual(expected);
     });
 
     test("별 뱃지 제공 테스트", () => {
         const decemberEvent = new DecemberEvent();
-        const badgeResult = decemberEvent.badgeEventCheck(5000);
+        const badgeResult = decemberEvent.getBadgeEventCheck(5000);
         const expected = '별';
         expect(badgeResult).toStrictEqual(expected);
     });
 
     test("트리 뱃지 제공 테스트", () => {
         const decemberEvent = new DecemberEvent();
-        const badgeResult = decemberEvent.badgeEventCheck(10000);
+        const badgeResult = decemberEvent.getBadgeEventCheck(10000);
         const expected = '트리';
         expect(badgeResult).toStrictEqual(expected);
     });
 
     test("산타 뱃지 제공 테스트", () => {
         const decemberEvent = new DecemberEvent();
-        const badgeResult = decemberEvent.badgeEventCheck(20000);
+        const badgeResult = decemberEvent.getBadgeEventCheck(20000);
         const expected = '산타';
         expect(badgeResult).toStrictEqual(expected);
     });
 
     test("뱃지 없음 제공 테스트", () => {
         const decemberEvent = new DecemberEvent();
-        const badgeResult = decemberEvent.badgeEventCheck(4999);
+        const badgeResult = decemberEvent.getBadgeEventCheck(4999);
         const expected = '';
         expect(badgeResult).toStrictEqual(expected);
     });
