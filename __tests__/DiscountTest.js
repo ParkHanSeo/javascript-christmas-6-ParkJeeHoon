ef5043ab804db584e961ea4d146635962bf7062a
ParkJeeHoon <gkstj8300@naver.com>
2023-11-15 11:09:32 +0900

    test: 특별 할인 계산 테스트 코드 작성

--- __tests__/DiscountTest.js
+++ __tests__/DiscountTest.js
@@ -41,4 +41,28 @@ describe("DecemberEvent 클래스 테스트", () => {
         const expected = 0;
         expect(discountResult).toStrictEqual(expected);
     });
+
+    test("특별 할인 계산 테스트 - 일요일 할인 적용", () => {
+        const discount = new Discount();
+        const orderMenu = new Order(('크리스마스파스타-1,초코케이크-1').split(',')).totalMenuOrderAmount();
+        const discountResult = discount.promotionTotalDiscount(10, orderMenu.ORDER).SPECIAL_DISCOUNT;
+        const expected = 1000;
+        expect(discountResult).toStrictEqual(expected);
+    });
+
+    test("특별 할인 계산 테스트 - 크리스마스 할인 적용", () => {
+        const discount = new Discount();
+        const orderMenu = new Order(('해산물파스타-2,레드와인-1').split(',')).totalMenuOrderAmount();
+        const discountResult = discount.promotionTotalDiscount(25, orderMenu.ORDER).SPECIAL_DISCOUNT;
+        const expected = 1000;
+        expect(discountResult).toStrictEqual(expected);
+    });
+
+    test("특별 할인 계산 테스트 - 할인 미적용", () => {
+        const discount = new Discount();
+        const orderMenu = new Order(('바비큐립-1,샴페인-1').split(',')).totalMenuOrderAmount();
+        const discountResult = discount.promotionTotalDiscount(30, orderMenu.ORDER).SPECIAL_DISCOUNT;
+        const expected = 0;
+        expect(discountResult).toStrictEqual(expected);
+    });
 });
